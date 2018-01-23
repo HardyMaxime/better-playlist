9032182f5e462b2a1d11011689c418ced460bf44
Maxime <maxime.hardy@etud.u-picardie.fr>
2018-01-23 12:00:42 +0100

    Add Data

--- src/App.js
+++ src/App.js
@@ -5,11 +5,61 @@ let defaultStyle = {
   color : '#fff'
 }
 
-class Aggregate extends Component {
+let fakeServerData = {
+  user : {
+    name : 'Maxime',
+    playlists : [
+      {
+        name : 'Justice songs',
+        songs :
+        [
+            {name:'D.A.N.C.E', duration: 180},
+            {name : 'Genesis', duration: 200},
+            {name : 'Phantom', duration : 175}
+        ]
+      },
+      {
+        name : 'Tame Impala songs',
+        songs :
+        [
+            {name:'Ouioui', duration: 180},
+            {name : 'song', duration: 200},
+            {name : 'U know', duration : 175}
+        ]
+      },
+      {
+        name : 'Ed Banger top songs',
+        songs :
+        [
+            {name:'technotronik', duration: 180},
+            {name : 'song', duration: 200},
+            {name : 'U know', duration : 175}
+        ]
+      }
+   ]
+ }
+}
+
+class PlaylistCounter extends Component {
   render() {
     return (
       <div className="aggregate">
-        <h2>Number Text</h2>
+        <h2>Number of playlists {this.props.playlists.length}</h2>
+      </div>
+    );
+  }
+}
+
+class HoursCounter extends Component {
+  render() {
+    let allSongs = this.props.playlists.reduce((songs, eachPlaylist) =>
+      { return songs.concat(eachPlaylist.songs) } , [])
+    let totalDuration = allSongs.reduce((sum,eachSong ) => {
+      return sum + eachSong.duration
+    }, 0)
+    return (
+      <div className="aggregate">
+        <h2>{Math.round(totalDuration/60)} hours</h2>
       </div>
     );
   }
@@ -44,13 +94,30 @@ class Playlist extends Component {
 }
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      serverData : {}
+    }
+  }
+  componentWillMount() {
+    this.setState({
+      serverData : fakeServerData
+    })
+  }
+
+  componentDidMount() {
+    console.log("Composant monté");
+  }
+
   render() {
     return (
       <div className="App">
         <header>
-          <h1>Titre</h1>
-          <Aggregate />
-          <Aggregate />
+          <h1>{this.state.serverData.user.name}'s Playlists</h1>p>
+          <PlaylistCounter playlists={this.state.serverData.user.playlists} />
+          <HoursCounter playlists={this.state.serverData.user.playlists} />
         </header>
         <div className="">
           <Filter />
